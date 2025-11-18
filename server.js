a55fc63ea7496a809cec2d78cd69b62edaf58591
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2025-11-18 18:21:29 +0500

    Updated server.js to load all libraries fast

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(__dirname));
+// Serve static files with proper MIME types
+app.use(express.static(__dirname, {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
 
-// Handle all routes by serving index.html
-app.get('*', (req, res) => {
+// Only serve index.html for HTML routes (not for static assets)
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
